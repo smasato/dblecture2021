438a7b6aac0c19aa13f16d7a83143c55270476ce
Masato Sugiyama <public@smasato.net>
2021-06-19 17:56:42 +0900

    add header item

--- header.php
+++ header.php
@@ -7,5 +7,8 @@
       <a href="http://turkey.slis.tsukuba.ac.jp/~s1711452/index.html" class="navbar-item">Top</a>
       <a href="http://turkey.slis.tsukuba.ac.jp/~s1711452/scores.php" class="navbar-item">楽譜</a>
     </div>
+    <div class="navbar-end">
+      <a href="http://turkey.slis.tsukuba.ac.jp/~s1711452/register_form.php" class="navbar-item">利用者登録</a>
+    </div>
   </div>
 </nav>
